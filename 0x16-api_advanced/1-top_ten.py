a5f939654892fe3499fb9579d90104133e367c5d
Obasi Ngozi <ndukaubangozi@gmail.com>
2024-05-09 11:50:17 +0100

    api_adv1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,21 @@
+#!/usr/bin/python3
+"""1-top_ten
+
+Run ./1-main.py programming or ./1-main.py this_is_a_fake_subreddit for testing
+"""
+
 import requests
 
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'CustomBot/1.0'}
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            if 'data' in data and 'children' in data['data']:
-                print("Titles of the first 10 hot posts in r/{}:".format(subreddit))
-                for post in data['data']['children']:
-                    print(post['data']['title'])
-            else:
-                print("None")
-        else:
-            print("None")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        print("None")
+    """Function that queries the Reddit API and prints the titles of the
+    first 10 hot posts listed for a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "1-top_ten.py"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200 and response.text.strip():
+        for post in response.json().get("data").get("children"):
+            print(post.get("data").get("title"))
+    else:
+        print(None)
